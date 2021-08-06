37c81604fba9842cf7baecb0802569c2581a10ca
torikatsu <torikatsu923@gmail.com>
2021-08-06 23:34:42 +0900

    wip creating spark state

--- lib/spark.dart
+++ lib/spark.dart
@@ -1,10 +1,54 @@
-class Spark {
-  Spark(this.x, this.y);
-  double x = 0;
-  double y = 0;
+import 'package:vector_math/vector_math.dart';
 
-  void advance() {
-    x += 10;
-    y += 10;
-  }
+class Spark {
+  Spark(this.mass, this.acceraration, this.velocity, this.position, this.);
+  final Vector3 acceraration;
+  final Vector3 velocity;
+  final Vector3 position;
+  final double mass;
+  final double ;
+
+  // 線香花火の燃焼時間は40sほど
+  // 線香花火の質量を0.5gとし、一定の速度で燃焼するとした時、
+  // 0.5g : 40s = mass g : lifetime
+  // より、
+  // lifetime = mass * 80
+  late final double lifetime = mass * 80;
+
+  List<Spark> advance() {}
+
+  // 速度を算出する
+  Vector3 _calculateVelocity(Vector3 v0, Vector3 a, double t) => Vector3(
+        _calculateVelocityOneDimentional(v0.x, a.x, t),
+        _calculateVelocityOneDimentional(v0.y, a.y, t),
+        _calculateVelocityOneDimentional(v0.z, a.z, t),
+      );
+
+  // 距離を算出する
+  Vector3 _calculatePosition(Vector3 p0, Vector3 v0, Vector3 a, double t) =>
+      Vector3(
+        _calculatePositionOneDimentional(p0.x, v0.x, t, a.x),
+        _calculatePositionOneDimentional(p0.y, v0.y, t, a.y),
+        _calculatePositionOneDimentional(p0.z, v0.z, t, a.z),
+      );
+
+  // t秒後の速度を算出する
+  // 以下で求めることができる
+  // v0: 初速度
+  // a: 加速度
+  // t: 時間
+  // v = v0*+at
+  double _calculateVelocityOneDimentional(double v0, double a, double t) =>
+      v0 + a * t;
+
+  // t秒間に移動する距離を算出する
+  // 移動する距離は以下で求めることができる
+  // v0: 初速度
+  // p0: 初期位置
+  // t: 時間
+  // a: 加速度
+  // p = p0 + v0t + 1/2at^2
+  double _calculatePositionOneDimentional(
+          double p0, double v0, double t, double a) =>
+      p0 + v0 * t + 0.5 * a * t * t;
 }
